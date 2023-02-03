ed1820dfbd3da51577fdcd63f883fd894d2f5bb6
Abhishek Raj <abhishekraj77001@gmail.com>
2023-02-03 15:07:15 +0530

    Title change

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('abhi-java-tutorial app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('abhi-java-tutorial application is running!');
   });
 });
